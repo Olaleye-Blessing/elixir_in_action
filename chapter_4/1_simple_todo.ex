ac34b23055678b61a36ec97ef1711c8cde344e5e
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-02-18 00:12:54 +0100

    Implement Read and Write Operation for TodoList

--- chapter_4/1_simple_todo.ex
+++ chapter_4/1_simple_todo.ex
@@ -1,21 +1,43 @@
 defmodule TodoList do
   @moduledoc false
 
-  alias Enumerable.Date
-  alias MultiDict
+  @type entry :: %{
+          date: any,
+          title: String.t()
+        }
 
-  @spec new() :: map
-  def new(),
-    do: MultiDict.new()
+  @type t :: %__MODULE__{auto_id: number(), entries: entry()}
 
-  @spec add_entry(list, any, String.t()) :: map
-  def add_entry(todo_list, date, title),
-    do: MultiDict.add(todo_list, date, title)
+  defstruct auto_id: 1, entries: %{}
 
-  @spec entries(list, any)
-  def entries(todo_list, date),
-    do: MultiDict.get(todo_list, date)
+  @spec new() :: t
+  @doc """
+  Instantaites a new %TodoList.
+  """
+  def new(), do: %TodoList{}
 
-  def due_today(todo_list),
-    do: entries(todo_list, "today's date")
+  @spec add_entry(t, entry) :: t
+  @doc """
+  Adds a new entry to %TodoList
+  """
+  def add_entry(todo_list, entry) do
+    # set the new entry's ID
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    # add the new entry to the entries list
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+
+    # update the struct
+    %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
+  end
+
+  @spec entries(t, any) :: [entry]
+  @doc """
+  Returns a list of todolists with the same date.
+  """
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
 end
