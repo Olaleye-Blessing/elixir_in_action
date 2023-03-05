73b34ee0000f39fa0b64df126a637de9ada29ae2
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-03-05 19:06:04 +0100

    Manipulate process state

---
+++ chapter_5/3_calculator.ex
@@ -0,0 +1,45 @@
+defmodule Calculator do
+  def start(), do: spawn(fn -> loop(0) end)
+
+  def loop(current_value) do
+    new_value =
+      receive do
+        {:value, caller} ->
+          send(caller, {:response, current_value})
+          # below needs to be returned. remember receive/0 returns the last computd expression
+          current_value
+
+        {:add, value} ->
+          current_value + value
+
+        {:sub, value} ->
+          current_value - value
+
+        {:mul, value} ->
+          current_value * value
+
+        {:div, value} ->
+          current_value / value
+
+        invalid_request ->
+          IO.puts("Invalid request #{inspect(invalid_request)}")
+          current_value
+      end
+
+    loop(new_value)
+  end
+
+  def value(server_pid) do
+    send(server_pid, {:value, self()})
+
+    receive do
+      {:response, value} ->
+        value
+    end
+  end
+
+  def add(server_pid, value), do: send(server_pid, {:add, value})
+  def sub(server_pid, value), do: send(server_pid, {:sub, value})
+  def mul(server_pid, value), do: send(server_pid, {:mul, value})
+  def div(server_pid, value), do: send(server_pid, {:div, value})
+end
